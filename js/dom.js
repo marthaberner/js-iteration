041092d8692573a525e285b9e3b1509d617c747a
marthaberner <marthaberner.mb@gmail.com>
2015-12-08 18:09:09 -0700

    User can see the sum of all prices

--- js/dom.js
+++ js/dom.js
@@ -4,8 +4,25 @@ Hints for the first problem:
 
   1. Find the button with the ID of `sum-all`
   2. Add an event listener that:
-    - finds the select element by name 
+    - finds the select element by name
     - passes all of its options to the `sum` function defined in `logic.js`
     - takes the value returned by the `sum` function and displays it on the page
 
 */
+
+// find the "sum all" button √
+// add event listener "click" √
+
+// get all prices values √
+// sum those values √
+// append the sum to the DOM
+
+
+var button = document.getElementById("sum-all");
+var prices = document.getElementsByClassName('all-prices')[0].options;
+var results = document.getElementById('results');
+
+
+button.addEventListener("click", function () {
+  results.innerHTML = "$" + getSum(prices);
+})
